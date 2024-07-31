ef1da7832cf3fcfeff7d42a83abc9609cdb1b381
Gabriel-analise <analise.gabriel@hotmail.com>
2024-07-31 19:09:04 -0300

    Atualizando dependências, configurações e o frontend

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        port: 3000, // porta para o front
+        proxy: {
+            '/': {
+                target: 'http://localhost:8001', // Porta do servidor Laravel
+                changeOrigin: true, // Se true, altera o cabeçalho Origin da solicitação para o destino do proxy. Útil para evitar problemas com CORS.
+                secure: false, // Define se o proxy deve ser seguro (usar HTTPS). Geralmente, false é adequado para desenvolvimento local.
+                ws: true, // Define se o proxy deve lidar com WebSockets. Pode ser útil dependendo da configuração do seu aplicativo.
+            },
+        },
+    },
 });
